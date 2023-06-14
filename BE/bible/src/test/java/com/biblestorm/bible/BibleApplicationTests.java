7ff5191fe4e80636ab081f00aaad8a92e18c4a83
anthony cecconi <anthony.cecconi@newtech-group.it>
2023-06-14 13:14:36 +0200

    add java-intelij-spring project

---
+++ BE/bible/src/test/java/com/biblestorm/bible/BibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblestorm.bible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
